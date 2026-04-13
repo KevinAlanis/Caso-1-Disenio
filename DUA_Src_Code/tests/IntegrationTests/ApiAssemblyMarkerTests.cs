7acf7ff2b29d019768dcf96a3e87a9be6f82b094
Kev Alanis <36685792+KevinAlanis@users.noreply.github.com>
2026-04-12 19:39:20 -0600

    Add files via upload

---
+++ DUA_Src_Code/tests/IntegrationTests/ApiAssemblyMarkerTests.cs
@@ -0,0 +1,10 @@
+namespace DUAStreamliner.IntegrationTests;
+
+public sealed class ApiAssemblyMarkerTests
+{
+    [Fact(Skip = "Skeleton project: add WebApplicationFactory tests when hosting is configured.")]
+    public void Placeholder()
+    {
+        // TODO: reference `Program` from the API host when integration tests are implemented.
+    }
+}
